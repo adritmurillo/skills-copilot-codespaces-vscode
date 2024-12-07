fedb704d644d4c7648501abaded98f78cb5b5e54
Joaquin Adrian Tumba Murillo <U20309516@utp.edu.pe>
2024-12-07 20:17:02 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,21 +1,8 @@
-// Create a web server
-// Create a route at /comments that returns a JSON object
-// The JSON object should have a key of "comments" and an array of comments as the value
-// The comments array should have at least 3 comments
-
+// Create web server
 const express = require('express');
 const app = express();
-
-app.get('/comments', (req, res) => {
-  res.json({
-    comments: [
-      { username: 'Tammy', comment: 'lololol' },
-      { username: 'Timmy', comment: 'lalalal' },
-      { username: 'Tommy', comment: 'lululul' }
-    ]
-  });
-});
-
-app.listen(3000, () => {
-  console.log('Server is listening on port 3000');
-});
+const bodyParser = require('body-parser');
+const fs = require('fs');
+const path = require('path');
+const port = 3000;
+const comments = require('./comments');
